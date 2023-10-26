61fbd84b2f925f2f0b71283bed23d0223d22606f
Larry Latorre <llatorre481@gmail.com>
2023-10-25 20:49:20 -0500

    lab-03

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # lab-02
+added two functions that asks questions and gives user x amount of guesses.
+Used some loops and arrays. 
+
+![Alt text](<Screenshot 2023-10-25 at 8.46.46 PM.png>)
--- js/app.js
+++ js/app.js
@@ -1,67 +1,122 @@
 'use strict';
-function askLocation (){
-  let location = prompt('Do I live in California? Y or N');
-  let locationAnswer = location.toUpperCase();
 
-  if (locationAnswer === "Y"){
+let correctGuesses = 0;
+
+
+function askLocation() {
+  let location = prompt('Am I from California? Y or N');
+  location = location.toUpperCase();
+
+
+  if (location === "Y") {
     alert('you are correct');
-  }else{
+    correctGuesses += 1;
+    console.log(correctGuesses);
+  } else {
     alert('wrong answer');
   }
 }
 
-function askCode (){
+function askCode() {
   let code = prompt('Am I learning how to code? Y or N');
-  let codeAnswer = code.toUpperCase();
+  code = code.toUpperCase();
 
-  if (codeAnswer === "Y"){
+  if (code === "Y") {
     alert('you are correct');
-  }else{
+    correctGuesses += 1;
+    console.log(correctGuesses)
+  } else {
     alert('wrong answer');
   }
 }
 
-function askPets (){
+function askPets() {
   let pet = prompt('Do I have any pets? Y or N');
-  let petAnswer = pet.toUpperCase();
+  pet = pet.toUpperCase();
 
-  if (petAnswer === "N"){
+  if (pet === "N") {
     alert('you are correct');
-  }else{
+    correctGuesses += 1;
+  } else {
     alert('wrong answer');
   }
 }
 
-function askBuild (){
+function askBuild() {
   let build = prompt('Have I ever helped build a home? Y or N');
-  let buildAnswer = build.toUpperCase();
+  build = build.toUpperCase();
 
-  if (buildAnswer === "Y"){
+  if (build === "Y") {
     alert('you are correct');
-  }else{
-    alert('wrong answer'); 
-  }
-}
-
-function askLanguage (){
-  let language = prompt('Do I speak another language? Y or N');
-  let languageAnswer = language.toUpperCase();
-
-  if (languageAnswer === "Y"){
-    alert('you are correct');
-  }else{
+    correctGuesses += 1;
+  } else {
     alert('wrong answer');
   }
 }
-function getName(){
+
+function askLanguage() {
+  let language = prompt('Do I speak another language? Y or N');
+  language = language.toUpperCase();
+
+  if (language === "Y") {
+    alert('you are correct');
+    correctGuesses += 1;
+  } else {
+    alert('wrong answer');
+  }
+}
+
+function years() {
+  let yearString = prompt('How many years have I lived in Oklahoma? You get 4 guesses.');
+  let yearNumber = parseInt(yearString);
+  let guess = 0;
+  for (let i = 0; i < 4; i++) {
+    if (yearNumber < 2) {
+      alert('Too low');
+      guess++;
+      yearNumber = prompt('try again');
+    } else if (yearNumber > 2) {
+      alert('Too high');
+      guess++;
+      yearNumber = prompt('try again');
+    } else {
+      alert('correct');
+      correctGuesses += 1;
+      guess++;
+      break;
+    }
+  } alert('You used ' + guess + ' guesses.');
+  return years;
+}
+
+function states() {
+  let guess = 0;
+  let statesLivedIn = ['CA', 'OR', 'NV', 'AZ', 'NM', 'TX', 'AR', 'OK'];
+  while (guess < 6) {
+    guess++;
+    let askState = prompt('Guess which States that I have lived in? Please use state abbreviations.You get 6 guesses.');
+    askState = askState.toUpperCase();
+    for (let i = 0; i < statesLivedIn.length; i++) {
+      if (askState === statesLivedIn[i]) {
+        alert('correct');
+      }
+    } break;
+  } 
+}
+
+
+function getName() {
   let userName = prompt('What is your name?');
   alert('Hello, ' + userName + ' welcome to my about me site.');
-  alert('Thanks for taking the time to get to know me ' + userName + '. I would like to get to know you too.' );
+  alert('You got ' + correctGuesses + ' guesses right.')
+  alert('Thanks for taking the time to get to know me ' + userName + '. I would like to get to know you too.');
   return userName;
 }
-askLocation();
-askCode();
-askPets();
-askBuild();
-askLanguage();
-getName();
+//askLocation();
+//askCode();
+//askPets();
+//askBuild();
+//askLanguage();
+//years();
+states();
+//getName();
